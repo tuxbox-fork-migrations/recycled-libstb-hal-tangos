7dc240d4d1bd3bbabc92eab33b6f2b0c6659ed29
TangoCash <eric@loxat.de>
2019-04-05 10:53:41 +0200

    fix build vuduo - not yet ready
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/libstb-hal-tangos/commit/5ea98b28517676bddf35cdf06451dff50110f35d
    Author: TangoCash <eric@loxat.de>
    Date: 2019-04-05 (Fri, 05 Apr 2019)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- libmipsbox/hardware_caps.c
+++ libmipsbox/hardware_caps.c
@@ -29,24 +29,7 @@ hw_caps_t *get_hwcaps(void)
 
 	memset(&caps, 0, sizeof(hw_caps_t));
 
-#if BOXMODEL_VUSOLO4K
-	initialized = 1;
-	caps.has_CI = 1;
-	caps.can_cec = 1;
-	caps.can_shutdown = 1;
-	caps.display_xres = 480;
-	caps.display_yres = 320;
-	caps.display_type = HW_DISPLAY_GFX;
-	caps.display_can_deepstandby = 1;
-	caps.display_can_set_brightness = 1;
-	caps.display_has_statusline = 1;
-	caps.has_button_timer = 1;
-	caps.has_HDMI = 1;
-	strcpy(caps.boxvendor, "VU+");
-	strcpy(caps.boxname, "SOLO4K");
-	strcpy(caps.boxarch, "BCM7376");
-#endif
-#if BOXMODEL_HD51
+#if BOXMODEL_VUDUO
 	initialized = 1;
 	caps.has_CI = 1;
 	caps.can_cec = 1;
@@ -58,41 +41,9 @@ hw_caps_t *get_hwcaps(void)
 	caps.display_has_statusline = 0;
 	caps.has_button_timer = 1;
 	caps.has_HDMI = 1;
-	strcpy(caps.boxvendor, "AX-Technologies");
-	strcpy(caps.boxname, "HD51");
-	strcpy(caps.boxarch, "BCM7251S");
-#endif
-#if BOXMODEL_HD60
-	initialized = 1;
-	caps.has_CI = 0;
-	caps.can_cec = 1;
-	caps.can_shutdown = 1;
-	caps.display_xres = 4;
-	caps.display_type = HW_DISPLAY_LED_NUM;
-	caps.display_can_deepstandby = 0;
-	caps.display_can_set_brightness = 1;
-	caps.display_has_statusline = 0;
-	caps.has_button_timer = 1;
-	caps.has_HDMI = 1;
-	strcpy(caps.boxvendor, "AX-Technologies");
-	strcpy(caps.boxname, "HD60");
-	strcpy(caps.boxarch, "HI3798M");
-#endif
-#if BOXMODEL_BRE2ZE4K
-	initialized = 1;
-	caps.has_CI = 1;
-	caps.can_cec = 1;
-	caps.can_shutdown = 1;
-	caps.display_xres = 5;
-	caps.display_type = HW_DISPLAY_LINE_TEXT;
-	caps.display_can_deepstandby = 0;
-	caps.display_can_set_brightness = 1;
-	caps.display_has_statusline = 1;
-	caps.has_button_timer = 1;
-	caps.has_HDMI = 1;
-	strcpy(caps.boxvendor, "WWIO");
-	strcpy(caps.boxname, "BRE2ZE4K");
-	strcpy(caps.boxarch, "BCM7251S");
+	strcpy(caps.boxvendor, "VU+");
+	strcpy(caps.boxname, "DUO");
+	strcpy(caps.boxarch, "BCM7335");
 #endif
 	return &caps;
 }
