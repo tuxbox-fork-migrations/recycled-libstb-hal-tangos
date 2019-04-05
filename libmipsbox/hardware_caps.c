9098afb74ffe1ca18252f42792dc97a26023d7bd
TangoCash <eric@loxat.de>
2019-04-05 13:11:23 +0200

    align to ddt git
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/libstb-hal-tangos/commit/b4d0f4bfe1fc78144622911628d673f2e06b1c2b
    Author: TangoCash <eric@loxat.de>
    Date: 2019-04-05 (Fri, 05 Apr 2019)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- libmipsbox/hardware_caps.c
+++ libmipsbox/hardware_caps.c
@@ -31,16 +31,17 @@ hw_caps_t *get_hwcaps(void)
 
 #if BOXMODEL_VUDUO
 	initialized = 1;
-	caps.has_CI = 1;
+	caps.has_CI = 2;
 	caps.can_cec = 1;
 	caps.can_shutdown = 1;
 	caps.display_xres = 16;
 	caps.display_type = HW_DISPLAY_LINE_TEXT;
-	caps.display_can_deepstandby = 0;
+	caps.display_can_deepstandby = 1;
 	caps.display_can_set_brightness = 1;
 	caps.display_has_statusline = 0;
 	caps.has_button_timer = 1;
 	caps.has_HDMI = 1;
+	caps.has_SCART = 1;
 	strcpy(caps.boxvendor, "VU+");
 	strcpy(caps.boxname, "DUO");
 	strcpy(caps.boxarch, "BCM7335");
