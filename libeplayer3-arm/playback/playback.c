59d8cf40cc9cdd9cb647ce8eebe6c350ce67d11b
Jacek Jendrzej <overx300@gmail.com>
2020-01-03 21:54:48 +0100

    libeplayer3-arm:playback.c reduce log errors
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/libstb-hal-tangos/commit/eeb456355e5b3ec32f8aa29fb7dd6e35b5275c0b
    Author: Jacek Jendrzej <overx300@gmail.com>
    Date: 2020-01-03 (Fri, 03 Jan 2020)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- libeplayer3-arm/playback/playback.c
+++ libeplayer3-arm/playback/playback.c
@@ -242,11 +242,14 @@ static int PlaybackClose(Context_t *context)
 	{
 		playback_err("container delete failed\n");
 	}
-
-	context->manager->audio->Command(context, MANAGER_DEL, NULL);
-	context->manager->video->Command(context, MANAGER_DEL, NULL);
-        context->manager->subtitle->Command(context, MANAGER_DEL, NULL);
-	context->manager->chapter->Command(context, MANAGER_DEL, NULL);
+	if(context->manager->audio)
+		context->manager->audio->Command(context, MANAGER_DEL, NULL);
+	if(context->manager->video)
+		context->manager->video->Command(context, MANAGER_DEL, NULL);
+	if(context->manager->subtitle)
+	        context->manager->subtitle->Command(context, MANAGER_DEL, NULL);
+	if (context->manager->chapter)
+		context->manager->chapter->Command(context, MANAGER_DEL, NULL);
 
 	context->playback->isPaused     = 0;
 	context->playback->isPlaying    = 0;
