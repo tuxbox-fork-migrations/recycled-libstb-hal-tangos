777924ea3be6d78643b0cbfdac4e516c1170d931
vanhofen <vanhofen@gmx.de>
2020-11-22 21:07:31 +0100

    hw_caps: introduce startup_file
    Signed-off-by: Thilo Graf <dbt@novatux.de>
    
    
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/libstb-hal-tangos/commit/d678c4083b531b229085ece5fd99df15a0fe650c
    Author: vanhofen <vanhofen@gmx.de>
    Date: 2020-11-22 (Sun, 22 Nov 2020)
    
    Origin message was:
    ------------------
    - hw_caps: introduce startup_file
    
    Signed-off-by: Thilo Graf <dbt@novatux.de>
    
    
    ------------------
    This commit was generated by Migit

--- libgeneric-pc/hardware_caps.c
+++ libgeneric-pc/hardware_caps.c
@@ -34,6 +34,8 @@ hw_caps_t *get_hwcaps(void)
 	caps.display_xres = 8;
 	caps.display_can_deepstandby = 0;
 	caps.display_has_statusline = 0;
+	caps.display_has_colon = 0;
+	strcpy(caps.startup_file, "");
 	strcpy(caps.boxvendor, "Generic");
 	strcpy(caps.boxname, "PC");
 	if (! uname(&u)){
