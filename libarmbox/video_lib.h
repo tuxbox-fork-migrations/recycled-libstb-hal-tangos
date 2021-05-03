8e5cf88b34fb9735fec69cac71bf48e6da0cda13
BPanther <bpanther_ts@hotmail.com>
2021-05-03 23:58:11 +0200

    vuduo4k/vuduo4kse/vuultimo4k/vuuno4kse: HDMI input as mode and PiP added
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/libstb-hal-tangos/commit/499dee4c8778aac4967765d60bf5da7c108d9c3d
    Author: BPanther <bpanther_ts@hotmail.com>
    Date: 2021-05-03 (Mon, 03 May 2021)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- libarmbox/video_lib.h
+++ libarmbox/video_lib.h
@@ -161,6 +161,7 @@ class cVideo
 	private:
 		/* video device */
 		int fd;
+		bool fdd;
 		unsigned int devnum;
 		/* apparently we cannot query the driver's state
 		   => remember it */
@@ -194,7 +195,6 @@ class cVideo
 
 		/* used internally by dmx */
 		int64_t GetPTS(void);
-
 	public:
 		/* constructor & destructor */
 		cVideo(int mode, void *, void *, unsigned int unit = 0);
@@ -203,6 +203,11 @@ class cVideo
 		/* used internally by playback */
 		void openDevice(void);
 		void closeDevice(void);
+
+		void open_AVInput_Device(void);
+		void close_AVInput_Device(void);
+
+		void setAVInput(int val);
 
 		void * GetTVEnc() { return NULL; };
 		void * GetTVEncSD() { return NULL; };
