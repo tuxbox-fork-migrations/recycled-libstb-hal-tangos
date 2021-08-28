969a75e68a43c3146fb5b54af481c635a4d28b33
Jacek Jendrzej <overx300@gmail.com>
2021-08-28 15:16:33 +0200

    build with ffmpeg version > 59.0.100
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/libstb-hal-tangos/commit/10cec9311ff60403cba8f659db4dcbd0b88ab0e6
    Author: Jacek Jendrzej <overx300@gmail.com>
    Date: 2021-08-28 (Sat, 28 Aug 2021)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- libarmbox/video.cpp
+++ libarmbox/video.cpp
@@ -227,7 +227,11 @@ void write_frame(AVFrame* in_frame, int fd)
 		return;
 	static const unsigned char pes_header[] = {0x0, 0x0, 0x1, 0xe0, 0x00, 0x00, 0x80, 0x80, 0x5, 0x21, 0x0, 0x1, 0x0, 0x1};
 
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(59,0,100)
 	AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_MPEG2VIDEO);
+#else
+	const AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_MPEG2VIDEO);
+#endif
 	if (codec)
 	{
 		AVCodecContext *codec_context = avcodec_alloc_context3(codec);
@@ -324,8 +328,12 @@ int decode_frame(AVCodecContext *codecContext,AVPacket &packet, int fd)
 
 AVCodecContext* open_codec(AVMediaType mediaType, AVFormatContext* formatContext)
 {
-	AVCodec * codec = NULL;
-	AVCodecContext * codecContext = NULL;
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(59,0,100)
+	AVCodec *codec = NULL;
+#else
+	const AVCodec *codec = NULL;
+#endif
+	AVCodecContext *codecContext = NULL;
 	int stream_index;
 #if (LIBAVFORMAT_VERSION_INT < AV_VERSION_INT( 57,25,101 ))
 	stream_index = av_find_best_stream(formatContext, mediaType, -1, -1, NULL, 0);
